d861d602c2784114dd1bb84b4c6ce101b2cc0e0e
monsterpc <maverina518@gmail.com>
2020-10-22 13:57:59 +0300

    Правки + удаление связей.

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import re
 import matplotlib.pyplot as plt
@@ -21,10 +23,12 @@ class Graph():
         print(self.graph_dict)
 
     def _calculate_graph(self):
-        self.graph_2d = np.array([[abs(a[1][0] - b[1][0]) + abs(a[1][1] - b[1][1])
-                                   for a in self.graph_dict.items()]
-                                  for b in self.graph_dict.items()]
-                                 )
+        self.graph_2d = [[abs(a[1][0] - b[1][0]) + abs(a[1][1] - b[1][1])
+                          for a in self.graph_dict.items()]
+                         for b in self.graph_dict.items()]
+
+        """for i in range(self.size):
+            self.graph_2d[i][i] = float('inf')"""
         print(self.graph_2d)
 
     def read_graph(self):
@@ -32,47 +36,73 @@ class Graph():
             self._create_graph_dict(f)
             self._calculate_graph()
 
+    # Функция нахождения минимального элемента, исключая текущий элемент
+    def Min(self, lst, myindex):
+        return min(x for idx, x in enumerate(lst) if idx != myindex)
+
+    # функция удаления нужной строки и столбцах
+    def Delete(self, matrix, index1, index2):
+        del matrix[index1]
+        for i in matrix:
+            del i[index2]
+        return matrix
+
     def get_paths(self):
         paths = []
         for i in range(0, 3):
             print(i)
-            path = self._find_path()
+            # https://habr.com/ru/post/329604/
+            # https://github.com/Clever-Shadow/python-salesman/blob/master/salesman.py
+            path = self.find_path_Kmean()
+            print(path)
             paths.append(path)
             self._del_path_to_graph(path)
 
         return self._paths_to_format(paths)
 
-    def _find_path(self):
-        n = self.size
-        #m = 100
-        ib = 3
+    def print_matrix(self, matrix):
+        print("---------------")
+        for i in range(len(matrix)):
+            print(matrix[i])
+        print("---------------")
+
+    def find_path_Kmean(self):
+        m = 100
+        # ib = 3
         way = []
         a = 0
-        #X = np.random.uniform(a, m, n)
-        #Y = np.random.uniform(a, m, n)
-        # X=[10, 10, 100,100 ,30, 20, 20, 50, 50, 85, 85, 75, 35, 25, 30, 47, 50]
-        # Y=[5, 85, 0,90,50, 55,50,75 ,25,50,20,80,25,70,10,50,100]
-        # n=len(X)
-        M = self.graph_2d  # Шаблон матрицы относительных расстояний между пунктами
-        """for i in np.arange(0, n, 1):
-            for j in np.arange(0, n, 1):
+        X = [x[1][0] for x in self.graph_dict.items()]
+        Y = [x[1][1] for x in self.graph_dict.items()]
+
+        M = np.zeros([self.size, self.size])  # Шаблон матрицы относительных расстояний между пунктами
+        for i in np.arange(0, self.size, 1):
+            for j in np.arange(0, self.size, 1):
                 if i != j:
-                    M[i, j] = sqrt((X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2)  # Заполнение матрицы
+                    M[i, j] = abs(X[i] - X[j]) + abs(Y[i] - Y[j])  # Заполнение матрицы
                 else:
-                    M[i, j] = float('inf')  # Заполнение главной диагонали матрицы"""
+                    M[i, j] = float('inf')  # Заполнение главной диагонали матрицы
+
+        ib = random.randint(1, self.size)
         way.append(ib)
-        for i in np.arange(1, n, 1):
+        for i in np.arange(1, self.size, 1):
             s = []
-            for j in np.arange(0, n, 1):
+            for j in np.arange(0, self.size, 1):
                 s.append(M[way[i - 1], j])
             way.append(s.index(min(s)))  # Индексы пунктов ближайших городов соседей
             for j in np.arange(0, i, 1):
-                M[way[i], way[j]] = float('inf')
-                M[way[i], way[j]] = float('inf')
+                M[way[i]][way[j]] = float('inf')
+                M[way[i]] [way[j]] = float('inf')
+
+        return way
+
+    def drow_path(self, X, Y, way, a, m, ib):
         S = sum([sqrt((X[way[i]] - X[way[i + 1]]) ** 2 + (Y[way[i]] - Y[way[i + 1]]) ** 2) for i in
-                 np.arange(0, n - 1, 1)]) + sqrt((X[way[n - 1]] - X[way[0]]) ** 2 + (Y[way[n - 1]] - Y[way[0]]) ** 2)
+                 np.arange(0, self.size - 1, 1)]) + sqrt(
+            (X[way[self.size - 1]] - X[way[0]]) ** 2 + (Y[way[self.size - 1]] - Y[way[0]]) ** 2)
+
         plt.title('Общий путь-%s.Номер города-%i.Всего городов -%i.\n Координаты X,Y случайные числа от %i до %i' % (
-        round(S, 3), ib, n, a, m), size=14)
+            round(S, 3), ib, self.size, a, m), size=14)
+        n = self.size
         X1 = [X[way[i]] for i in np.arange(0, n, 1)]
         Y1 = [Y[way[i]] for i in np.arange(0, n, 1)]
         plt.plot(X1, Y1, color='r', linestyle=' ', marker='o')
@@ -85,7 +115,13 @@ class Graph():
         plt.show()
 
     def _del_path_to_graph(self, path):
-        pass
+        for i_town, town in enumerate(path):
+            if i_town != len(path) - 1:
+                self.graph_2d[town][path[i_town + 1]] = 0
+                self.graph_2d[path[i_town + 1]][town] = 0
+            else:
+                self.graph_2d[town][0] = 0
+                self.graph_2d[0][town] = 0
 
     def _paths_to_format(self, paths):
         pass
